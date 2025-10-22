39f9003ea04a37249176f10ecfe9ee89440c5356
duonglkh <duonglkh@cemsoftwareltd.com>
2025-10-22 14:49:17 +0700

    update data

--- gun_simulator/sound/index.js
+++ gun_simulator/sound/index.js
@@ -1,7 +1,7 @@
-System.register("chunks:///sound.js",[],(function(){return{execute:function(){}}}));
+System.register("bundle://sound/sound.js",[],(function(){return{execute:function(){}}}));
 
 (function(r) {
-  r('virtual:///prerequisite-imports/sound', 'chunks:///sound.js'); 
+  r('virtual:///prerequisite-imports/sound', 'bundle://sound/sound.js'); 
 })(function(mid, cid) {
     System.register(mid, [cid], function (_export, _context) {
     return {
